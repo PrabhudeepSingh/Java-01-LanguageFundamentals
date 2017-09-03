ec7bf5541d3487994a88dbf3bc4be30cbe383301
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-03 22:48:00 +0530

    To illustrate the concept of Identifies in java.

---
+++ Identifier.java
@@ -0,0 +1,25 @@
+public class Identifier
+{
+	public static void main(String[] args) {
+        int x = 10;
+        int total123 = 20; //alpha-numeric identifiers are allowed
+
+        //int 123total = 30; identifiers should not start with digit
+
+        int number = 30;
+        int Number = 40;
+        int NUMBER = 50; //These shows that java is a 
+ 
+       // int if=60; can't use reservad word as identifiers
+
+        int String = 60;
+
+        System.out.println("value of x = "+x);                //10
+        System.out.println("value of total123 = "+total123);  //20 
+        System.out.println("value of number = "+number);      //30
+        System.out.println("value of Number = "+Number);      //40 
+        System.out.println("value of NUMBER = "+NUMBER);      //50
+        System.out.println("value of String = "+String);      //60
+    
+ }    
+}
