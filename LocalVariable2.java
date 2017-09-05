58c96e24289fc7d79bd38cf6f05655604172306a
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-05 09:26:23 +0530

    To illustrate the concept of Local Variables in java.

---
+++ LocalVariable2.java
@@ -0,0 +1,12 @@
+public class Localvariable2{
+     public static void main(String[] args)
+     {
+         int x;
+         if(args.length>0)
+         {
+             x = 10;
+         }
+        // System.out.println(x) -> variable x might have not been initialized
+     }
+    
+}
