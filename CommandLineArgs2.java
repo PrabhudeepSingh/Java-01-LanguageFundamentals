432f154fdfc210908df5ff680258747b18188968
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-07 16:34:18 +0530

    To illustrate the concept of Command Line Arguments in java.

---
+++ CommandLineArgs2.java
@@ -0,0 +1,10 @@
+public class CommandLineArgs2{
+    public static void main(String[] args) {
+                String[] argh={"a","b","c","d"};
+                args=argh;
+                for(String s:args)
+                {
+                    System.out.println(s);
+                }     
+    }
+}
